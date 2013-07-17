1019219ba8948e653e27070138614a6a1a947794
Sarah Woodrow <sarah.woodrow08@imperial.ac.uk>
2013-07-17 14:14:30 +0100

    Adding method templates to DataPoint class
    Renamed DataSet class to DataPoint class. Started filling in blank templates for methods that will be required

---
+++ C#/Spectroscopy Viewer/Spectroscopy Viewer/DataPoint.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spectroscopy_Viewer
+{
+
+    // Class to contain raw data for each frequency data point, and methods to access
+    public class DataPoint
+    {
+
+        // Private members:
+       
+
+
+
+        // 100 raw data readings
+
+        // Default constructor
+        public DataPoint()
+        {
+           
+        }
+
+
+        // Important things go here
+
+
+        // Set methods
+
+        // Method to set the cooling count threshold for calculating bad counts
+        public void setCoolThresh(int thresh)
+        {
+
+        }
+
+        // Get methods
+
+        // Method to return the frequency of the data point
+        public int getFreq()
+        {
+        }
+
+        // Method to return the number of bad counts
+        public int getBadCounts()
+        {
+        }
+
+        // Method to return excitation probability
+        public int getExcitation()
+        {
+            // Must be 
+        }
+
+
+
+
+
+
+    }
+}
