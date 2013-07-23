705dffe74ec5559d15d91a9f96cd21172c341377
Sarah Woodrow <sarah.woodrow08@imperial.ac.uk>
2013-07-23 19:06:29 +0100

    Added method to handle changes in selected index of combo boxes
    Need to work out how to disable one item in a combo box. Maybe remove it (or somehow keep it in the list but stop it from being shown). Or add a symbol at the beginning of the entry and check for that symbol then stop the user from choosing it.

--- C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.cs
+++ C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.cs
@@ -12,7 +12,7 @@ namespace Spectroscopy_Viewer
     public partial class spectrumSelect : Form
     {
         // List to display 
-        private BindingList<string> myListOfSpectra = new BindingList<string>();
+        private BindingList<string>[] myListOfSpectra;
         // Store the number of existing spectra
         private int existingSpectra = new int();
         // Store the number of interleaved spectra in file
@@ -37,16 +37,24 @@ namespace Spectroscopy_Viewer
             int numberInterleaved = numberInterleavedPassed;    // Store number of spectra in the file
             existingSpectra = mySpectrum.Count();               // Store number of existing spectra
             selectedSpectrum = new int[numberInterleaved];      // Initialise array
-            
+
+            // Create lists for drop-down boxes
+            myListOfSpectra = new BindingList<string>[numberInterleaved];
+            myListOfSpectra[0] = new BindingList<string>();
 
             // Create new item in list for each existing spectrum
             for (int i = 0; i < existingSpectra; i++)
             {
                 spectrumNames[i] = mySpectrum[i].getName();                     // Retrieve name of spectrum
-                myListOfSpectra.Add("Spectrum " + i + " (" + spectrumNames[i] + ")");    // Concatenate string with name & number
+                myListOfSpectra[0].Add("Spectrum " + (i+1) + " (" + spectrumNames[i] + ")");    // Concatenate string with name & number
             }
-            myListOfSpectra.Add("");    // Add a blank option
+            myListOfSpectra[0].Add("");    // Add a blank option
 
+            // Duplicate list
+            for (int i = 1; i < numberInterleaved; i++)
+            {
+                myListOfSpectra[i] = myListOfSpectra[0];
+            }
 
             // Set defaults for text box
             newSpectrumNameBox.MaxLength = 100;          // Set a sensible maximum length for spectrum name
@@ -67,7 +75,7 @@ namespace Spectroscopy_Viewer
             for (int i = 0; i < numberInterleaved; i++)
             {
                 myComboBoxLabel[i] = new Label();
-                myComboBoxLabel[i].Text = "Spectrum " + i + ":";
+                myComboBoxLabel[i].Text = "Spectrum " + (i+1) + ":";
                 myComboBoxLabel[i].Location = new Point(200, 10 + i * 30);
                 myComboBoxLabel[i].Size = new System.Drawing.Size(70, 13);
                 this.Controls.Add(myComboBoxLabel[i]);
@@ -77,6 +85,8 @@ namespace Spectroscopy_Viewer
                 myComboBox[i].Location = new Point(280, 10 + i * 30);
                 myComboBox[i].Size = new System.Drawing.Size(220, 21);
                 this.Controls.Add(myComboBox[i]);
+                this.myComboBox[i].SelectedIndexChanged +=
+                    new System.EventHandler(this.myComboBox_SelectedIndexChanged);
             }
             //********************************//
 
@@ -96,9 +106,11 @@ namespace Spectroscopy_Viewer
             // Add name to temporary list of new spectra to be created
             newSpectra.Add(newSpectrumNameBox.Text);
 
-            // Add spectra to list
-            myListOfSpectra.Add("(New) " + newSpectrumNameBox.Text);
-            
+            // Add spectra to all lists
+            for (int i = 0; i < numberInterleaved; i++)
+            {
+                myListOfSpectra[i].Add("(New) " + newSpectrumNameBox.Text);
+            }
             // NB drop-down lists automatically update
 
         }
@@ -109,9 +121,6 @@ namespace Spectroscopy_Viewer
             // For each of the interleaved spectra
             for (int i = 0; i < numberInterleaved; i++)
             {
-                // Assign which spectrum each data set should belong to - from user input on form
-                selectedSpectrum[i] = myComboBox[i].SelectedIndex;
-
                 if (selectedSpectrum[i] >= existingSpectra)
                 {
                     // Re-order so that the next spectrum to be dealt with is the next in the array
@@ -122,6 +131,33 @@ namespace Spectroscopy_Viewer
 
             this.Close();       // Close form
         }
+
+        // Handles change of selected index for all combo boxes
+        private void myComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // For each drop-down box
+            for (int i = 0; i < numberInterleaved; i++)
+            {
+                // Assign which spectrum each data set should belong to - from user input on form
+                selectedSpectrum[i] = myComboBox[i].SelectedIndex;
+            }
+
+            // For each drop-down box
+            for (int i = 0; i < numberInterleaved; i++)
+            {
+                // Check other drop-down boxes
+                for (int j = 0; j < numberInterleaved; i++)
+                {
+                    // Ignore selection made by this combo box
+                    if (j != i)
+                    {
+                        // Disable item in list that corresponds to selectedSpectrum[j]
+                    }
+                }
+            }
+
+        }
+
 
     }
 }
