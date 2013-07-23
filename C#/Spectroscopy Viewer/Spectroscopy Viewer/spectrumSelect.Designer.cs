e8c8eda9074655828331b7d9c646247c7c3c78cf
Sarah Woodrow <sarah.woodrow08@imperial.ac.uk>
2013-07-23 16:29:48 +0100

    Changed spectrumSelect form to handle multiple spectra
    Not quite finished

--- C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.Designer.cs
+++ C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.Designer.cs
@@ -28,70 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.spectrumSelectList = new System.Windows.Forms.ListBox();
-            this.addToSpectrumButton = new System.Windows.Forms.Button();
             this.newSpectrumNameBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.addNewSpectrumButton = new System.Windows.Forms.Button();
+            this.spectrumSelectDataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SourceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DestinationColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.spectrumSelectDataGrid)).BeginInit();
             this.SuspendLayout();
-            // 
-            // spectrumSelectList
-            // 
-            this.spectrumSelectList.FormattingEnabled = true;
-            this.spectrumSelectList.Location = new System.Drawing.Point(12, 12);
-            this.spectrumSelectList.Name = "spectrumSelectList";
-            this.spectrumSelectList.Size = new System.Drawing.Size(187, 134);
-            this.spectrumSelectList.TabIndex = 0;
-            // 
-            // addToSpectrumButton
-            // 
-            this.addToSpectrumButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.addToSpectrumButton.Location = new System.Drawing.Point(12, 170);
-            this.addToSpectrumButton.Name = "addToSpectrumButton";
-            this.addToSpectrumButton.Size = new System.Drawing.Size(187, 36);
-            this.addToSpectrumButton.TabIndex = 1;
-            this.addToSpectrumButton.Text = "Add to spectrum";
-            this.addToSpectrumButton.UseVisualStyleBackColor = true;
-            this.addToSpectrumButton.Click += new System.EventHandler(this.addToSpectrumButton_Click);
             // 
             // newSpectrumNameBox
             // 
-            this.newSpectrumNameBox.Location = new System.Drawing.Point(234, 31);
+            this.newSpectrumNameBox.Location = new System.Drawing.Point(426, 50);
             this.newSpectrumNameBox.Name = "newSpectrumNameBox";
             this.newSpectrumNameBox.Size = new System.Drawing.Size(186, 20);
             this.newSpectrumNameBox.TabIndex = 2;
+            this.newSpectrumNameBox.Text = "Enter spectrum name";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 12);
+            this.label1.Location = new System.Drawing.Point(426, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(179, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Please enter name of new spectrum:";
             // 
-            // addNewSpectrumButton
+            // spectrumSelectDataGrid
             // 
-            this.addNewSpectrumButton.Location = new System.Drawing.Point(234, 58);
-            this.addNewSpectrumButton.Name = "addNewSpectrumButton";
-            this.addNewSpectrumButton.Size = new System.Drawing.Size(75, 23);
-            this.addNewSpectrumButton.TabIndex = 4;
-            this.addNewSpectrumButton.Text = "Add to list";
-            this.addNewSpectrumButton.UseVisualStyleBackColor = true;
-            this.addNewSpectrumButton.Click += new System.EventHandler(this.addNewSpectrumButton_Click);
+            this.spectrumSelectDataGrid.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.spectrumSelectDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.spectrumSelectDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SourceColumn,
+            this.DestinationColumn});
+            this.spectrumSelectDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.spectrumSelectDataGrid.Name = "spectrumSelectDataGrid";
+            this.spectrumSelectDataGrid.Size = new System.Drawing.Size(304, 221);
+            this.spectrumSelectDataGrid.TabIndex = 5;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(117, 275);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 6;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // SourceColumn
+            // 
+            this.SourceColumn.HeaderText = "Source";
+            this.SourceColumn.Name = "SourceColumn";
+            // 
+            // DestinationColumn
+            // 
+            this.DestinationColumn.HeaderText = "Destination";
+            this.DestinationColumn.Name = "DestinationColumn";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // spectrumSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 420);
-            this.Controls.Add(this.addNewSpectrumButton);
+            this.ClientSize = new System.Drawing.Size(629, 420);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.spectrumSelectDataGrid);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.newSpectrumNameBox);
-            this.Controls.Add(this.addToSpectrumButton);
-            this.Controls.Add(this.spectrumSelectList);
             this.Name = "spectrumSelect";
             this.Text = "Choose spectrum";
+            ((System.ComponentModel.ISupportInitialize)(this.spectrumSelectDataGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,10 +116,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox spectrumSelectList;
-        private System.Windows.Forms.Button addToSpectrumButton;
         private System.Windows.Forms.TextBox newSpectrumNameBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button addNewSpectrumButton;
+        private System.Windows.Forms.DataGridView spectrumSelectDataGrid;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SourceColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn DestinationColumn;
+        private System.Windows.Forms.Button button1;
     }
 }
--- C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.cs
+++ C#/Spectroscopy Viewer/Spectroscopy Viewer/spectrumSelect.cs
@@ -12,7 +12,7 @@ namespace Spectroscopy_Viewer
     public partial class spectrumSelect : Form
     {
         // List to display 
-        private List<string> myList = new List<string>();
+        private List<string> myListOfSpectra = new List<string>();
         // Store the number of existing spectra
         private int existingSpectra = new int();
         // Store the number of interleaved spectra in file
@@ -40,23 +40,43 @@ namespace Spectroscopy_Viewer
             for (int i = 0; i < mySpectrum.Count; i++)
             {
                 spectrumNames[i] = mySpectrum[i].getName();                     // Retrieve name of spectrum
-                myList.Add("Spectrum " + i + " (" + spectrumNames[i] + ")");    // Concatenate string with name & number
+                myListOfSpectra.Add("Spectrum " + i + " (" + spectrumNames[i] + ")");    // Concatenate string with name & number
             }
 
-            // Option to create new spectrum
-            // NB put this last, then we can tell if a new or existing spectrum has been selected
-            myList.Add("Create new spectrum...");
 
-            // Populate list box from this list
-            spectrumSelectList.DataSource = myList;
-
-            // Set default text for box
-            newSpectrumNameBox.Text = "Spectrum name";
+            // Set defaults for text box
             newSpectrumNameBox.MaxLength = 100;          // Set a sensible maximum length for spectrum name
+            
+            // Set data source for combo box 
+            DestinationColumn.DataSource = myListOfSpectra;
+
+
+            for (int i = 0; i < numberInterleaved; i++)
+            {
+                // Add rows to data grid view
+                spectrumSelectDataGrid.Rows.Add();
+                spectrumSelectDataGrid.Rows[i].Cells[0].Value = i;
+            }
+
+
  
         }
 
-        private void addToSpectrumButton_Click(object sender, EventArgs e)
+
+        private void addNewSpectrumButton_Click(object sender, EventArgs e)
+        {
+            // Add name to temporary list of new spectra to be created
+            newSpectra.Add(newSpectrumNameBox.Text);
+
+            // Add spectra to list
+            myListOfSpectra.Add(newSpectrumNameBox.Text + " (New)");
+            
+            // Trying to get this to update.. but it doesn't seem to be working
+            // Wait for new form design before worrying too much
+            this.Refresh();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
         {
             // Temp variables for debugging
             selectedSpectrum[0] = 0;
@@ -69,33 +89,19 @@ namespace Spectroscopy_Viewer
                 // Check which spectrum each data set should belong to - from user input on form
                 // (Waiting for form design)
 
-                
+
 
 
                 if (selectedSpectrum[i] >= existingSpectra)
                 {
                     // Re-order so that the next spectrum to be dealt with is the next in the array
                     // This ensures correct metadata when items are added to list using List.Add
-                    spectrumNames.Add( newSpectra[selectedSpectrum[i] - existingSpectra] );
-                 }
+                    spectrumNames.Add(newSpectra[selectedSpectrum[i] - existingSpectra]);
+                }
             }
 
             this.Close();       // Close form
         }
-
-        private void addNewSpectrumButton_Click(object sender, EventArgs e)
-        {
-            // Add name to temporary list of new spectra to be created
-            newSpectra.Add(newSpectrumNameBox.Text);
-
-            // Add spectra to list
-            myList.Add(newSpectrumNameBox.Text + " (New)" );
-            
-            // Trying to get this to update.. but it doesn't seem to be working
-            // Wait for new form design before worrying too much
-            this.Refresh();
-        }
-
 
 
     }
